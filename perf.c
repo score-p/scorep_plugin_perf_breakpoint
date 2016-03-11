baed033b4eb61cdd1e05c849e9f7fb94b8ab6e69
Philipp Trommler <philipp.trommler@mailbox.tu-dresden.de>
2016-03-11 12:51:35 +0100

    Added/Corrected licence files and file headers and pulled submodule to most recent version.

--- perf.c
+++ perf.c
@@ -1,7 +1,28 @@
 /*
-libbp.so,
-a library to count accesses to a variable in Score-P and VampirTrace
-Copyright (C) 2015 TU Dresden, ZIH
+ * Copyright (c) 2016, Technische Universität Dresden, Germany
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ *    and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse
+ *    or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #define PACKAGE 1
@@ -123,7 +144,7 @@ void build_perf_attr(struct perf_event_attr * attr, const char * complex_name) {
         free(table);
         if (attr->type == PERF_TYPE_BREAKPOINT)
         {
-          bfd_close(bfd_variable); 
+          bfd_close(bfd_variable);
           return;
         }
         storage_needed = bfd_get_dynamic_symtab_upper_bound(bfd_variable);
